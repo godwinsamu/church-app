a2098347fba956519e57da7c64ac59a15d77e7d2
godwinsamu <164744237+godwinsamu@users.noreply.github.com>
2024-08-31 18:22:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function confirm(){
-    window.alert("You have become a member of this church");
+const symbol = document.querySelector(".symbol");
+const navlinks = document.querySelector("ul"); 
+symbol.addEventListener("click",()=>{
+navlinks.classList.toggle("nav");
+});
+function join(){
+    alert("Congradulations you have joined as a member of the Church . Stay Blessed");
+}
+function contact(){
+    alert("Your message have been sent to the officials . Stay tuned for their reply");
 }
